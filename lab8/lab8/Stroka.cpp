b0fe9cf5b33168d27128252f891652bd9dcf0826
Павел Мухлаев <pavelsmutniy@gmail.com>
2026-03-18 20:23:23 +0300

    Add files via upload

---
+++ lab8/lab8/Stroka.cpp
@@ -0,0 +1,147 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Stroka.h"
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+// Глобальные вспомогательные функции
+int myStrlenGlobal(const char* str) {
+    int length = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        length++;
+    }
+    return length;
+}
+
+void myStrcpyGlobal(char* dest, const char* src) {
+    int i = 0;
+    while (src[i] != '\0') {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+}
+
+void myStrcatGlobal(char* dest, const char* src) {
+    int destLen = myStrlenGlobal(dest);
+    int i = 0;
+    while (src[i] != '\0') {
+        dest[destLen + i] = src[i];
+        i++;
+    }
+    dest[destLen + i] = '\0';
+}
+
+int myStrcmpGlobal(const char* str1, const char* str2) {
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (str1[i] < str2[i]) return -1;
+        if (str1[i] > str2[i]) return 1;
+        i++;
+    }
+
+    if (str1[i] == '\0' && str2[i] == '\0') return 0;
+    if (str1[i] == '\0') return -1;
+    return 1;
+}
+
+// Реализация методов класса Stroka
+
+Stroka::Stroka() {
+    length = 0;
+    str = new char[1];
+    str[0] = '\0';
+}
+
+Stroka::Stroka(const char* s) {
+    length = myStrlenGlobal(s);
+    str = new char[length + 1];
+    myStrcpyGlobal(str, s);
+}
+
+Stroka::Stroka(const Stroka& s) {
+    length = s.length;
+    str = new char[length + 1];
+    myStrcpyGlobal(str, s.str);
+}
+
+Stroka::Stroka(int size) {
+    length = 0;
+    str = new char[size + 1];
+    str[0] = '\0';
+}
+
+Stroka::~Stroka() {
+    delete[] str;
+}
+
+Stroka& Stroka::operator=(const Stroka& s) {
+    if (this == &s) {
+        return *this;
+    }
+
+    delete[] str;
+    length = s.length;
+    str = new char[length + 1];
+    myStrcpyGlobal(str, s.str);
+
+    return *this;
+}
+
+Stroka Stroka::operator+(const Stroka& s) const {
+    Stroka result;
+    delete[] result.str;
+
+    result.length = length + s.length;
+    result.str = new char[result.length + 1];
+
+    myStrcpyGlobal(result.str, str);
+    myStrcatGlobal(result.str, s.str);
+
+    return result;
+}
+
+int Stroka::operator==(const Stroka& s) const {
+    return (myStrcmpGlobal(str, s.str) == 0) ? 1 : 0;
+}
+
+int Stroka::dlinia() const {
+    return length;
+}
+
+void Stroka::myStrcpy(const char* src) {
+    delete[] str;
+    length = myStrlenGlobal(src);
+    str = new char[length + 1];
+
+    int i = 0;
+    while (src[i] != '\0') {
+        str[i] = src[i];
+        i++;
+    }
+    str[i] = '\0';
+}
+
+// ПЕРЕГРУЗКА ОПЕРАТОРОВ ВВОДА/ВЫВОДА (ЗАДАНИЕ 1)
+
+// Оператор вывода (<<)
+ostream& operator<<(ostream& os, const Stroka& s) {
+    os << s.str;
+    return os;
+}
+
+// Оператор ввода (>>)
+istream& operator>>(istream& is, Stroka& s) {
+    char buffer[256];  // Временный буфер для ввода
+
+    // Читаем строку (максимум 255 символов)
+    is >> buffer;
+
+    // Освобождаем старую память и создаем новую строку
+    delete[] s.str;
+    s.length = myStrlenGlobal(buffer);
+    s.str = new char[s.length + 1];
+    myStrcpyGlobal(s.str, buffer);
+
+    return is;
+}
